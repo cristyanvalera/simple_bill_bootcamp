71c59d00b827b1d2ad75254277fb9ef391cec623
Cristyan Valera <cristyan.valera.81@gmail.com>
2023-02-06 19:55:08 -0400

    some minor changes

--- factura.py
+++ factura.py
@@ -1,56 +1,82 @@
-
 from datetime import datetime
 
+inventory = [
+    {"id": 1, "name": "cafe", "price": 12.04, "stock": 25},
+    {"id": 2, "name": "azucar", "price": 8.75, "stock": 30},
+    {"id": 3, "name": "mantequilla", "price": 7.50, "stock": 24},
+    {"id": 4, "name": "queso", "price": 14.99, "stock": 34},
+    {"id": 5, "name": "harina", "price": 3.45, "stock": 48},
+]
 
-encabezado = '''
+def show_products(inventory):
+    for product in inventory:
+        id, name, price = product["id"], product["name"], product["price"]
+        print(f"{id} - {name.title():15} ${price}")
+
+
+header = '''
 ------------------------------------
+             SENIAT
            CBC MARKET
-      - LISTA DE LA COMPRA -
+          J-40563258-8
+  CALLE 18, ENTRE CARRERAS 5 Y 6
+       GUANARE - PORTUGUESA
+------------------------------------'''
+
+print(header)
+print(f"     {datetime.today()}")
+print("------------------------------------")
+
+client = input("Nombre del cliente: ").capitalize()
+document = input("Documento del cliente: ")
+
+print('''
 ------------------------------------
-'''
+    -- MENU DE PRODUCTOS --
+------------------------------------''')
 
-print(encabezado)
-shopping = []
+ask = input("¿Desea ver la lista de productos disponibles? S/N: ").lower()
+if ask == "s":
+    show_products(inventory)
 
-# Datos del cliente
-name = input("Nombre del cliente: ")
-document = input("Documento de identidad: ")
-print()
+shopping_car = []
+subtotal = 0
+iva = 0
+total = 0
 
-print("Para salir del programa escriba 's'\n")
+print("-----------------------------------")
+salir = input("Para salir del programa escriba 'q'\n")
+if salir == "q":
+    quit()
+
+# print("Para salir del programa escriba 'q'\n")
 while True:
-    product = input("Ingrese un producto: ")
-    if product.lower() == "s":
+    buscar = input("--> ")
+    if buscar.lower() == "q":
         break
-    else:
-        price = float(input(f"Precio de {product}: $"))
-        shopping.append({"product": product, "price": price})
 
-detalle = f'''
-------------------------------------------------------
-                    -- SENIAT -- 
-------------------------------------------------------
-{datetime.now()}
+    for product in inventory:
+        if buscar.lower() == product["name"]:
+            shopping_car.append({"name": product["name"], 
+                                 "price": product["price"]})
+        elif buscar.lower() == "q":
+            break
 
-Cliente: {name.capitalize()}
-Documento: {document}
-------------------------------------------------------
-            -- DETALLE DE LA COMPRA -- 
-------------------------------------------------------
-'''
-
-print()
-print(detalle)
-
-for prod in shopping:
-    print("{}: ${}".format(prod["product"].title(), prod["price"]))
-
-subtotal = sum(product['price'] for product in shopping)
+subtotal = sum(product['price'] for product in shopping_car)
 iva = subtotal * .16
 total = subtotal + iva
 
-print("------------------------------------------------------")
+print(header)
+print(f"Cliente: {client}")
+print(f"Documento: {document}")
+print("-----------------------------------")
 
-print(f"SubTotal: ${subtotal}")
-print(f"IVA:      ${iva}")
-print(f"Total:    ${total}")
+for product in shopping_car:
+    name, price = product["name"], product["price"]
+    print("{:<26} ${:<9}".format(name.title(), price))
+
+print("-----------------------------------")
+print(f"SubTotal:", " " * 16, f"${subtotal:.2f}")
+print(f"IVA 16%: ", " " * 16, f"${iva:.2f}")
+print("-----------------------------------")
+print("Total:    ", " " * 15, f"${total:.2f}")
